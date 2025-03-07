fb07cf260c9e60fa0dc0f1655df38b076e7de609
sawayamakouji <sawayamakouji@gmail.com>
2025-03-07 23:34:55 +0900

    AAA

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html'], // public フォルダ内のすべてのHTMLファイルを対象
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
